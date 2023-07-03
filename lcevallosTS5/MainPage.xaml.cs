c35adf17c6a2b7515bd5c335a1964b37c346b498
LECT <LECT@DESKTOP-8TV9SIH>
2023-07-03 13:40:19 -0500

    Agregar archivos de proyecto.

---
+++ lcevallosTS5/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace lcevallosTS5
+{
+    public partial class MainPage : ContentPage
+    {
+        private string Url = "http://172.20.3.52/rutasegura/post.php";
+        private HttpClient cliente = new HttpClient();
+        private ObservableCollection<Vehiculo> post;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+             
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var contenido = await cliente.GetStringAsync(Url);
+            List<Vehiculo> listaPost = JsonConvert.DeserializeObject<List<Vehiculo>>(contenido);
+            post = new ObservableCollection<Vehiculo>(listaPost);
+            listaVehiculos.ItemsSource = post;
+        }
+
+        private async void btnMostrar_Clicked(object sender, EventArgs e)
+        {
+            var contenido = await cliente.GetStringAsync(Url);
+            List<Vehiculo> listaPost = JsonConvert.DeserializeObject<List<Vehiculo>>(contenido);
+            post = new ObservableCollection<Vehiculo>(listaPost);
+            listaVehiculos.ItemsSource = post;
+        }
+    }
+}
---
+++ lcevallosTS5/Vehiculo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lcevallosTS5
+{
+    public class Vehiculo
+    {
+        public int id_vehiculo { set; get; }
+        public string marca { set; get; }
+        public string modelo { set; get; }
+        public string color { set; get; }
+        public string placa { set; get; }
+        public string tipo_vehiculo { set; get; }
+    }
+}
